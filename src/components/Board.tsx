e64def08a65a09254396e6beda35efe97a919348
VictoriaShirleyUnifei <d2023002387@unifei.edu.br>
2025-09-07 23:59:56 -0300

    ajusta lógica de right click

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,42 +3,56 @@ import { openSquare, toggleFlag, type BoardType } from "../util/logic";
 import Square from "./Square";
 import "./styles.css";
 
-type Props = {  
+type Props = {
   board: BoardType;
   setBoard: (board: BoardType) => void;
-  setStatus: (status: "playing" | "lost"| "won") => void;
+  setStatus: (status: "playing" | "lost" | "won") => void;
   onReset: () => void;
+  status: string;
 };
 
-const Board: React.FC<Props> = ({ board, setBoard, setStatus, onReset }) => { 
+const Board: React.FC<Props> = ({
+  board,
+  setBoard,
+  setStatus,
+  onReset,
+  status,
+}) => {
   const handleClick = (row: number, col: number) => {
+    if (status !== "playing") return;
     const result = openSquare(board, row, col);
     setBoard(result.board);
     setStatus(result.status);
-
-    
-    
-    
+  };
+  
+  const handleRightClick = (row: number, col: number) => {
+    if (status !== "playing") return;
+    const newBoard = toggleFlag(board, row, col);
+    setBoard(newBoard);
   };
 
   return (
     <>
-    <div className="board" style={{ gridTemplateColumns: `repeat(${board[0].length}, 2.5rem)` }}>
-      {board.map((row, rIdx) =>
-        row.map((square, cIdx) => (
-          <Square
-            key={`${rIdx}-${cIdx}`}
-            square={square}
-            onClick={() => handleClick(rIdx, cIdx)} 
-            onRightClick={() => setBoard(toggleFlag(board, rIdx, cIdx))}
-          />
-        ))
-      )}
-    </div>
-    <button className="botao"  onClick={onReset}>🔄 Reiniciar Jogo</button> 
+      <div
+        className="board"
+        style={{ gridTemplateColumns: `repeat(${board[0].length}, 2.5rem)` }}
+      >
+        {board.map((row, rIdx) =>
+          row.map((square, cIdx) => (
+            <Square
+              key={`${rIdx}-${cIdx}`}
+              square={square}
+              onClick={() => handleClick(rIdx, cIdx)}
+              onRightClick={() => handleRightClick(rIdx, cIdx)}
+            />
+          ))
+        )}
+      </div>
+      <div className="botao">
+        <button onClick={onReset}>🔄 Reiniciar Jogo</button>{" "}
+      </div>
     </>
   );
 };
 
 export default Board;
-
