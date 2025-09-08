203e7218909ed0b85410020ee9030a18265a5e88
Matheus Vilas Bôas Brito <153239530+odebig@users.noreply.github.com>
2025-09-07 23:24:43 -0300

    cria botao de reiniciar o jogo

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,23 +6,39 @@ import "./styles.css";
 type Props = {  
   board: BoardType;
   setBoard: (board: BoardType) => void;
+  setStatus: (status: "playing" | "lost"| "won") => void;
+  onReset: () => void;
 };
 
-const Board: React.FC<Props> = ({ board, setBoard }) => {
+const Board: React.FC<Props> = ({ board, setBoard, setStatus, onReset }) => { 
+  const handleClick = (row: number, col: number) => {
+    const result = openSquare(board, row, col);
+    setBoard(result.board);
+    setStatus(result.status);
+
+    
+    
+    
+  };
+
   return (
+    <>
     <div className="board" style={{ gridTemplateColumns: `repeat(${board[0].length}, 2.5rem)` }}>
       {board.map((row, rIdx) =>
         row.map((square, cIdx) => (
           <Square
             key={`${rIdx}-${cIdx}`}
             square={square}
-            onClick={() => setBoard(openSquare(board, rIdx, cIdx))}
+            onClick={() => handleClick(rIdx, cIdx)} 
             onRightClick={() => setBoard(toggleFlag(board, rIdx, cIdx))}
           />
         ))
       )}
     </div>
+    <button className="botao"  onClick={onReset}>🔄 Reiniciar Jogo</button> 
+    </>
   );
 };
 
 export default Board;
+
